5d538dc6a70c745278f153349b2ece3e8da5caa3
Perry Craft <ljcraft12@gmail.com>
2017-03-13 10:43:23 -0400

    added app.component and app.module ts files

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'myApp',                                                                                                  // Change the name of myApp to anything you want it to be
+    template: '<h1>Angular 2 Skeleton Project </h1>'
+})
+
+export class AppComponent { }
