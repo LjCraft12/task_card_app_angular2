9b2ab3034800ea8c25796b76bc8f64c8dccfa9da
Perry Craft <ljcraft12@gmail.com>
2017-03-13 11:07:28 -0400

    added html index file

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 @NgModule({
     imports:      [ BrowserModule ],
     declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    bootstrap:    [ AppComponent ]                                                                                      // This is not Twitter Bootstrap
 })
 
 export class AppModule { }
