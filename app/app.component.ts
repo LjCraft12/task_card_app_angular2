c5c8196ce3f7aeba7a3479d1683271bac0a31475
Perry Craft <ljcraft12@gmail.com>
2017-03-13 22:20:53 -0400

    creating task card app first commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Skeleton Project</h1>'
+    template: '<h1>Task Card App</h1>'
 })
 export class AppComponent { }
