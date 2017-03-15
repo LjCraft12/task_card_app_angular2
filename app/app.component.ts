8ed8733f7e143b9a310bd503ffca0e92a85ed7ec
Perry Craft <ljcraft12@gmail.com>
2017-03-15 13:43:39 -0400

    hardcoded task for cards and used *ngFor to iterate  through the array of card components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Task } from './model/task'
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    private tasks = [                                                                                                    // always put private in front of your component properties so nothing can modify the data out side of it
+        new Task(
+            "Buy a monkey",
+            false
+        ),
+        new Task(
+            "Study more TypeScript",
+            false
+        )
+    ]
+}
